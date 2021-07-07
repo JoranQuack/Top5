525c7908c9e4b24dab628bd21eede91eb578232f
Joran Le Quellec <joran.lequellec@gmail.com>
2021-07-07 13:53:53 +1200

    scrollTo with Alex

---
+++ scrollTo.js
@@ -0,0 +1,9 @@
+$(function () {
+    $(".headerLink").on("click", function (e) {
+      e.preventDefault();
+      var offset = $(window).height() * 0.13;
+      var target = this.hash;
+      var topPos = $(target).position().top;
+      window.scrollTo(0, topPos - offset);
+    });
+  });
