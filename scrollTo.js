fef47a4e88288cfb5e7e25301271e903c2ea5fe3
Joran Le Quellec <joran.lequellec@gmail.com>
2021-07-30 10:20:40 +1200

    Retired teachers and started new thing

--- scrollTo.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $(".headerLink").on("click", function (e) {
-      e.preventDefault();
-      var offset = $(window).height() * 0.13;
-      var target = this.hash;
-      var topPos = $(target).position().top;
-      window.scrollTo(0, topPos - offset);
-    });
-  });
